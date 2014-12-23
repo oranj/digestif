f03449501b8958c88391b36dbac0fb714bd598b3
Ray Minge <ray@rayminge.com>
2014-12-23 13:32:49 -0600

    First commit\!

---
+++ README.md
@@ -0,0 +1,64 @@
+# grunt-digestif
+
+> Creates a heirarchical digest of a folder using AMD for uncompiled library loading.
+
+## Getting Started
+This plugin requires Grunt `~0.4.5`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-digestif --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-digestif');
+```
+
+## The "digestif" task
+
+### Overview
+In your project's Gruntfile, add a section named `digestif` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  digestif: {
+    targetName: {
+      path: './path/to/project/root',
+      prefix: 'myapp',
+      loadLibraries: ['yourapp/YourApp', 'hisapp/HisApp'],
+      ignore: ['r']
+    }]
+  }
+});
+```
+
+### Digests
+
+You may have any number of projects to digest. They will be executed in order.
+
+#### digest.path
+Type: `String`
+Required
+
+A path to the directory you want digested. Relative to the project root.
+
+#### digest.prefix
+Type: `String`
+Required
+
+This is your AMD path for this directory.
+
+#### digest.ignore
+Type: `Array[string]`
+Optional
+
+This allows you to skip modules (do not put a .js)
+
+#### digest.loadLibraries
+Type: `Array[string]`
+Optional
+
+Allows you to specify other libraries to load by package name.
---
+++ tasks/digestif.js
@@ -0,0 +1,113 @@
+/*
+ * grunt-digestif
+ * https://github.com/oranj/digestif
+ *
+ * Copyright (c) 2014 Ray Minge
+ * Licensed under the MIT license.
+ */
+
+module.exports = function(grunt) {
+	'use strict';
+
+	grunt.registerMultiTask('digestif', 'Creates a heirarchical digest of a folder using AMD for uncompiled library loading.', function() {
+
+		var path = this.data.path,
+			outputName = this.target,
+			postLoadLibraries = this.data.loadLibraries || [],
+			skipFiles = this.data.ignore || [],
+			prefix = this.data.prefix,
+			outputFilePath = path + '/' + outputName + ".js";
+
+		removeFile(outputFilePath);
+
+		argIndex = 0;
+		functionDefines = [];
+		bodyString = "\treturn {";
+		functionArgs = [];
+		digestRecursive(path, prefix, "\n\t\t", postLoadLibraries, skipFiles);
+		bodyString += "\n\t};";
+
+		var str = "define([\""+functionDefines.join("\",\"")+"\"], function("+functionArgs.join(",")+"){\n\t\"use strict\";\n"+bodyString+"\n});\n";
+
+		grunt.log.writeln("Writing file:" + outputFilePath);
+		grunt.verbose.writeln(str);
+
+		writeFile(outputFilePath, str);
+	});
+
+	var fs = require('fs'),
+		argIndex,
+		functionArgs,
+		functionDefines,
+		bodyString, digestRecursive, removeFile, writeFile, digest;
+
+	digestRecursive = function(path, defNamePrefix, pathPrefix, postLoadLibraries, skipFiles) {
+		var contents = fs.readdirSync(path),
+			libName, modName, count = 0, argName,
+			i, fullPath, packagePath, stats, dirs = [], files = [];
+
+		for (i = 0; i < contents.length; i++) {
+			if (contents[i].charAt(0) == '.') {
+				continue;
+			}
+			stats = fs.statSync(path + '/' + contents[i]);
+			if (stats.isDirectory()) {
+				dirs.push(contents[i]);
+			} else if (modName = contents[i].match(/^(.*?)\.js$/)) {
+				files.push(modName[1]);
+			}
+		}
+
+		for (i = 0; i < files.length; i++) {
+			if (skipFiles.indexOf(files[i]) >= 0) {
+				grunt.verbose.writeln("Skipping file: " + path + '/' + files[i]);
+				continue;
+			}
+			if (count > 0) {
+				bodyString += ",";
+			}
+			argName = "a"+String(argIndex);
+			functionArgs.push(argName),
+			functionDefines.push(defNamePrefix + "/" + files[i]);
+			bodyString += pathPrefix + files[i] + ":"+argName;
+			argIndex++;
+			count ++;
+		}
+		for (i = 0; i < dirs.length; i++) {
+			if (count > 0) {
+				bodyString += ",";
+			}
+			bodyString += pathPrefix + dirs[i] + ": {";
+			digestRecursive(path + "/" + dirs[i], defNamePrefix + "/" + dirs[i], pathPrefix + "\t", [], skipFiles);
+			bodyString += pathPrefix + "}";
+			count++;
+		}
+
+		for (var i = 0; i < postLoadLibraries.length; i++) {
+			if (count > 0) {
+				bodyString += ",";
+			}
+
+			argName = "a" +String(argIndex);
+			libName = postLoadLibraries[i].split('/');
+			libName = libName[libName.length - 1];
+			functionArgs.push(argName);
+			functionDefines.push(postLoadLibraries[i]);
+			bodyString += pathPrefix + libName + ":" + argName;
+			argIndex++;
+			count++;
+		}
+	};
+
+	removeFile = function(fileName) {
+		if (fs.existsSync(fileName)) {
+			grunt.log.writeln("Removing file:" + fileName);
+			fs.unlinkSync(fileName);
+		}
+	};
+
+	writeFile = function(fileName, contents) {
+		fs.writeFileSync(fileName, contents);
+	};
+
+};
